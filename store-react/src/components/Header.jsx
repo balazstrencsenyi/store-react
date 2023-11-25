80a9bc6d87ff502ccf6d21d47fcd587e6034bee8
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-11-25 14:30:50 +0100

    add header component

---
+++ store-react/src/components/Header.jsx
@@ -0,0 +1,22 @@
+function Header () {
+  return (
+    <div className="headerContainer">
+      <div className="menuContainer">
+        <ul>
+          <img src="./src/assets/apple.svg" className="appleLogo" />
+          <li>Store</li>
+          <li>Mac</li>
+          <li>iPad</li>
+          <li>iPhone</li>
+          <li>Watch</li>
+          <li>AirPods</li>
+          <li>Accessories</li>
+          <li>Support</li>
+          <img src="./src/assets/search.svg" className="search" />
+        </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header;
--- store-react/vite.config.js
+++ store-react/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import ViteFontsPlugin from "vite-plugin-fonts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ViteFontsPlugin({
+      google: {
+        families: [
+          {
+            name: "Roboto",
+            styles: "400,700",
+          },
+        ],
+      },
+    }),
+  ],
+});
