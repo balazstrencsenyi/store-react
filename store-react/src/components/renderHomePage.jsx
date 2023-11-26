d58c61c1975642d8a16ece9640dde531d8293b0a
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-11-26 15:10:27 +0100

    add slides

--- store-react/src/components/renderHomePage.jsx
+++ store-react/src/components/renderHomePage.jsx
@@ -17,9 +17,15 @@ function RenderHomePage() {
 
   return (
     <div className='homePageContainer'>
-      <div className="firstSlide">first slide..</div>
-      <div className="secondSlide">second slide..</div>
-      <div className="thirdSlide">third slide..</div>
+      <div className="firstSlide">
+        <img src="./src/assets/slide1.png" className='slide1' />
+      </div>
+      <div className="secondSlide">
+        <img src="./src/assets/slide2.png" className='slide2' />
+      </div>
+      <div className="thirdSlide">
+        <img src="./src/assets/slide3.png" className='slide3' />
+      </div>
     </div>
   );
 }
